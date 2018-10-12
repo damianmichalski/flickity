6d8a2de5237f222a0bd395ffcd1cb09ea41972ce
DamianM <damian.michalski@kramp.com>
2018-10-12 07:43:25 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  wrapAround: true,
+  pageDots: false,
+  contain: true
+});
+
