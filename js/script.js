35f651887630d8758ecd2ac90a3ab6b74498f5d6
DamianM <damian.michalski@kramp.com>
2018-10-16 15:50:28 +0200

    delete white space

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // external js: flickity.pkgd.js
 
 var carousel = document.querySelector('.carousel');
-var flkty = new Flickity( carousel, {
+var flkty = new Flickity(carousel, {
     initialIndex: 1,
     pageDots: false,
     hash: true,
@@ -9,15 +9,13 @@ var flkty = new Flickity( carousel, {
 });
 
 var buttons = document.querySelector(".button");
-buttons.addEventListener( 'click', function( event ) {
+buttons.addEventListener('click', function (event) {
     flkty.selectCell(0);
 });
 
 var progressBar = document.querySelector('.progress-bar')
 
-flkty.on( 'scroll', function( progress ) {
-    progress = Math.max( 0, Math.min( 1, progress ) );
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
-
-
