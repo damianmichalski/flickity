8f1b3e66165209fa6fd2f976cd80963a19a83816
DamianM <damian.michalski@kramp.com>
2018-10-17 15:07:54 +0200

    mustache

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-// external js: flickity.pkgd.js
+// external js: flickity.pkgd.js+
+
+var templateItem = document.getElementById('template-slider').innerHTML;
+Mustache.parse(templateItem);
+var listItems = '';
+
+for(var i = 0; i < productsData.length; i++){
+    console.log(productsData);
+    listItems += Mustache.render(templateItem, productsData[i]);
+}
+results.insertAdjacentHTML('beforeend', listItems);
 
 var carousel = document.querySelector('.carousel');
 var flkty = new Flickity(carousel, {
-    initialIndex: 1,
+    imagesLoaded: true,
+    percentPosition: false,
     pageDots: false,
     hash: true,
+    contain: true,
     cellAlign: 'left'
 });
 
@@ -19,3 +31,4 @@ flkty.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
+
